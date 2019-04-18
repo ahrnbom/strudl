edad1a825adfb1ce28bffd9580c828f0c078f4e1
Hakan Ardo <ardo@maths.lth.se>
2019-04-18 13:27:10 +0200

    data path

--- run_docker.sh
+++ run_docker.sh
@@ -11,5 +11,5 @@ echo "Using data from $DATAPATH"
 
 docker build -t strudl .
 docker rm -f strudl_sess
-nvidia-docker run --name strudl_sess -p 80:80 -v $PWD:/code/ -v $DATAPATH:/data/ --mount type=bind,src=/media,dst=/usb,bind-propagation=rslave -it strudl bash
+nvidia-docker run --name strudl_sess -p 80:80 -v $PWD:/code/ -v $DATAPATH:/code/data/ --mount type=bind,src=/media,dst=/usb,bind-propagation=rslave -it strudl bash
 # Mounting /media as an rslave allows STRUDL to access USB drives inserted after the docker image starts. A simple -v does not support this, for some reason.
